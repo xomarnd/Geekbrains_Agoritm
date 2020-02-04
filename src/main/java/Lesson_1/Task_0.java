96ffa941ea518331caaa7b9ff8d3e42a61e937c4
smirnov <8c5LxlqoE>
2020-02-04 21:58:56 +0300

    Task0

--- src/main/java/Lesson_1/Task_0.java
+++ src/main/java/Lesson_1/Task_0.java
@@ -3,6 +3,6 @@ package Lesson_1;
 public class Task_0 {
     public static void main(String[] args) {
         int taskNumber = 0;
-        System.out.println ("Task " + taskNumber);
+        System.out.println ("Task " + taskNumber );
     }
 }
