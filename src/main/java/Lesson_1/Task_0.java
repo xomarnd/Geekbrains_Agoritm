4d8f43375330cecb6390b8148e698f56541de44c
smirnov <8c5LxlqoE>
2020-02-09 21:02:28 +0300

    Task2

--- src/main/java/Lesson_1/Task_0.java
+++ src/main/java/Lesson_1/Task_0.java
@@ -1,8 +1,44 @@
 package Lesson_1;
 
+//  Есть массив целых чисел. Найти все тройки чисел, которые в сумме дают ноль.
+
+// Сложность - O(n3)
+
 public class Task_0 {
+    private static int ARRAYLENGTH = 60;
+    private static int MININT = 20;
+    private static int MAXINT = -3;
+
     public static void main(String[] args) {
-        int taskNumber = 0;
-        System.out.println ("Task " + taskNumber);
+        int[] myArray = new int[ARRAYLENGTH];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = rnd(MININT, MAXINT);
+        }
+
+        sum(myArray);
+
+    }
+    private static void sum(int[] arr){
+        int count = 0;
+        for (int a = 0; a < arr.length; a++) {
+            for (int b = a + 1; b < arr.length; b++) {
+                for (int c = b + 1; c < arr.length; c++) {
+                    if(arr[a] + arr[b] + arr[c] == 0){
+                        count++;
+                        System.out.println (arr[a] + "+" + arr[b] + "+" + arr[c] + " = 0;");
+                    }
+                }
+            }
+        }
+        if (count == 0){
+            System.out.println ("Нет результата");
+        }else{
+            System.out.println ("\nНайдено: " + count);
+        }
+    }
+
+    public static int rnd(int min, int max){
+        max -= min;
+        return (int) (Math.random() * ++max) + min;
     }
 }
--- src/main/java/Lesson_1/Task_1.java
+++ src/main/java/Lesson_1/Task_1.java
@@ -2,6 +2,8 @@ package Lesson_1;
 
 //Есть массив целых чисел. Найти два наибольших значения в массиве
 
+// Сложность - n
+
 public class Task_1 {
     private static int ARRAYLENGTH = 20;
     private static int MININT = -6;
@@ -12,7 +14,30 @@ public class Task_1 {
             for (int i = 0; i < myArray.length; i++) {
                 myArray[i] = rnd(MININT, MAXINT);
             }
+            twoMax(myArray);
+    }
+    public static void twoMax(int[] arr) {
+        if (arr.length < 2) {
+            System.out.println("Invalid array length");
+            return;
+        }
 
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        for (int i=0; i < arr.length; i++) {
+            if (arr[i] > first) {
+                second = first;
+                first = arr[i];
+            } else if (arr[i] > second && arr[i] != first) {
+                second = arr[i];
+            }
+        }
+
+        if (second == Integer.MIN_VALUE) {
+            System.out.println("Нет второго максимального значения");
+        } else {
+            System.out.printf("Первое значение: " + first + "\nВторое значение: " + second);
+        }
     }
 
     public static int rnd(int min, int max){
